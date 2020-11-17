6e34426bf2e216067d9adcf8fb253b97a5ba3bc7
Eric <eric.m.lombardo@gmail.com>
2020-11-17 12:13:19 -0500

    commit tree 1fd6640b77375816384e96e0857166cd7b533f26

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(names)
+  # go through each item and print list w/ numbers
+  names.each_with_index { | name, index | puts "#{index + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  # take each item, capitalize it, add elemation point, return new array
+  planeteer_calls.collect { | call | "#{call.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { | call | call.length > 4}    # check calls array for any item longer than 4
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(recipe)
+  # search through recipe, see if ingredient matches any of cheeses, return first ingredient that does
   cheese_types = ["cheddar", "gouda", "camembert"]
+  recipe.find { | ingredient | cheese_types.include?(ingredient) ? ingredient : nil }
 end
+
+#find through each one, check to see if item.include?(cheeses), if true return item
